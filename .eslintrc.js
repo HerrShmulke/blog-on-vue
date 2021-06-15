b58d952d62a42a8a7c9d13698994ce7b3f482c36
HerrShmulke <30705015+HerrShmulke@users.noreply.github.com>
2021-06-15 23:39:58 +0700

    feat: more config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,12 @@ module.exports = {
     node: true,
   },
 
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/prettier'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/prettier',
+    'plugin:vue/vue3-recommended',
+  ],
 
   parserOptions: {
     parser: 'babel-eslint',
@@ -15,6 +20,32 @@ module.exports = {
     'no-console': 'off',
     'no-debugger': 'off',
     'vue/html-comment-indent': 'error',
+    'vue/require-name-property': 'error',
+    'vue/no-boolean-default': ['error', 'default-false'],
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      {
+        registeredComponentsOnly: false,
+        ignores: [],
+      },
+    ],
+    'vue/v-on-event-hyphenation': ['error', 'always'],
+    'vue/v-on-function-call': ['error', 'never'],
+    'vue/padding-line-between-blocks': ['error', 'always'],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 3,
+          allowFirstLine: true,
+        },
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
   },
 
   overrides: [
