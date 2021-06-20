a8cda21f3560e2458e010bbac65b539418392968
HerrShmulke <30705015+HerrShmulke@users.noreply.github.com>
2021-06-21 03:05:33 +0700

    feat: add base container

---
+++ src/components/ui/VContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div :class="$style.container">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" module>
+.container {
+  max-width: 980px;
+  margin: 0 auto;
+}
+</style>
