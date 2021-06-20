2e5d973a4981cc56a119176250994c4480c6bb74
HerrShmulke <30705015+HerrShmulke@users.noreply.github.com>
2021-06-21 03:05:50 +0700

    feat: add base icon button

---
+++ src/components/ui/VButtonIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <button :class="[$style.buttonIcon]" type="button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'VButtonIcon',
+};
+</script>
+
+<style lang="scss" module>
+.buttonIcon {
+  background: none;
+  cursor: pointer;
+  display: flex;
+  border: none;
+  padding: 0;
+  margin: 0;
+}
+</style>
